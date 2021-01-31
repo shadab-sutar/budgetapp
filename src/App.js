fae29916b3bf50da594430a48f37175cee5ccc15
SH <SH@Shadabs-MacBook-Air.local>
2021-01-31 20:08:03 +0530

    home page design draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '50%', float: 'left', margin: '1px', background: '#f2445b', textAlign: 'center' }}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Current Expenditure
+        </Typography>
+          <Typography variant="h5" component="h2">
+            &#8377; 2,000
+        </Typography>
+        </CardContent>
+      </Card>
+      <Card style={{ width: '50%', float: 'left', margin: '1px', background: '#bcf078', textAlign: 'center' }}>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Remaining Balance
+        </Typography>
+          <Typography variant="h5" component="h2">
+            &#8377; 2,000
+        </Typography>
+        </CardContent>
+      </Card>
+
+      <AppBar position="fixed" color="primary" style={{ bottom: '0', top: 'auto', height: '50px' }}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton edge="end" color="inherit">
+            <MoreIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
